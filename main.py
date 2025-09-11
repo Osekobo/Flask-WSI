e1ddc39f67827ac37627358db8c014031fbe4d9c
Osekobo <collinsboseko2005@gmail.com>
2025-09-11 17:09:10 +0300

    flaskAPIs

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+products_list = []
+sales_list = []
+purchase_list = []
 
 @app.route("/")
 def home():
   res = {"Flask API Version" : "1.0"}
   return jsonify(res), 200
 
+@app.route("/api/products", methods = ["GET", "POST"])
+def products():
+  if request.method == "GET":
+    return jsonify(products_list), 200
+  elif request.method == "POST":
+    data = dict(request.get_json())
+    if "name" not in data.keys()  or "buying_price" not in  data.keys() or "selling_price" not in data.keys():
+      error = {"error" : "Ensure  all fields are set"} 
+      return jsonify(error), 400
+    else:
+     products_list.append(data)
+     return jsonify(data), 201
+  else:
+    error = {"error" : "Method not allowed"}
+    return jsonify(error), 405
+  
 app.run()
