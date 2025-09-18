36d4ad547d07e5982d072eda75fcd9c28f29e82c
Osekobo <collinsboseko2005@gmail.com>
2025-09-18 14:40:44 +0300

    Flask_JWT

--- main.py
+++ main.py
@@ -1,22 +1,64 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 
-products_list = []
+jwt = JWTManager(app)
+app.config['JWT_SECRET_KEY'] = 'hgyutd576uyfutu'  # Change this to a random secret key in production
+
+products_list = []  
 sales_list = []
 purchase_list = []
 sales_list = []
 purchases_list = []
+users_list = []
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
   res = {"Flask API Version" : "1.0"}
   return jsonify(res), 200
 
+
+@app.route("/api/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    if "name" not in data.keys() or "email" not in data.keys() or "password" not in data.keys():
+        error = {"error": "Ensure all fields are filled"}
+        return jsonify(error), 400
+        #Elif expected to check mail is valid/exists, password is long, fields not empty
+    else:
+        users_list.append(data)
+        #create JWT token
+        token = create_access_token(identity=data["email"])
+        return jsonify({"token": token}), 201
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    if "email" not in data.keys() or "password" not in data.keys():
+        error = {"error": "Ensure all fields are filled"}
+        return jsonify(error), 400
+    else:
+       for user in users_list:
+            if user["email"] == data["email"] and user["password"] == data["password"]:
+                #create JWT token
+                token = create_access_token(identity = data["email"])
+                return jsonify({"token":token}), 200
+            else:
+                 pass
+       error = {"error": "Invalid email or password"}
+       return jsonify(error), 401
+
+@app.route("/api/users")
+def get_users():
+    return jsonify(users_list), 200
+
 @app.route("/api/products", methods = ["GET", "POST"])
+@jwt_required
 def products():
   if request.method == "GET":
+    # retrive products
     return jsonify(products_list), 200
   elif request.method == "POST":
     data = dict(request.get_json())
@@ -33,6 +75,7 @@ def products():
   
 # sales - product_id(int), quantity(float), created_at(datetime_now)
 @app.route("/api/sales", methods = ["GET", "POST"])
+@jwt_required
 def sales():
   if request.method == "GET":
     return jsonify(sales_list), 200
@@ -52,6 +95,7 @@ def sales():
 
 # purchases - product_id(int), quantity(float), created_at(datetime_now)
 @app.route("/api/purchases", methods = ["GET", "POST"])
+@jwt_required
 def purchases():
   if request.method == "GET":
     return jsonify(purchases_list), 200
@@ -60,7 +104,7 @@ def purchases():
     if "created_at" not in data:
       data["created_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     if "product_id" not in data.keys()   or "quantity" not in  data.keys():
-      error = {"error" : "Ensure  all fields are set and with correct input types"} 
+      error = {"error" : "Ensure  all fields are set and with correct input types"}
       return jsonify(error), 400
     else:
      purchases_list.append(data)
@@ -77,3 +121,8 @@ app.run()
 # 2. Always return data as JSON / Capture as JSON
 # 3. Specify the request method e.g GET, POST, PUT, DELETE, PATCH
 # 4. Return status Code (used by an application that is consuming)
+
+# JWT is JSON Web Token - Generatednin the API and sent to the client
+# A  Client(web,mobile) can not access a protected route without a token
+# The client stores that toejn once they login or register
+# pip install flask-jwt-extended
